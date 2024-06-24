a179ca61ccef4498214357c1117f52746865bb56
ANTHONY LIMA E SILVA <72108893+Anthony-a-l-s@users.noreply.github.com>
2024-06-24 19:13:16 -0300

    estilizando paginas login e register

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,37 @@
+import LogoImg from '../../assets/logo.svg'
+import { Link } from 'react-router-dom';
+import { FiUser, FiLogIn } from 'react-icons/fi';
+
 
 export function Header() {
-
+    const signed = false;
+    const loadingAuth = false;
 
     return (
-        <div>
-            <h1>Componente header</h1>
+        <div className='w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4'>
+            <header className='flex w-full max-w-7xl items-center justify-between px-4 mx-auto'>
+                <Link to='/'>
+                    <img src={LogoImg}
+                        alt='Logo do site'
+                    />
+                </Link>
+
+                {!loadingAuth && signed && (
+                    <Link to='/dashboard'>
+                        <div className='border-2 rounded-full p-1 border-gray-900'>
+                            <FiUser size={22} color='#000' />
+                        </div>
+                    </Link>
+                )}
+
+                {!loadingAuth && !signed && (
+                    <div className='border-2 rounded-full p-1 border-gray-900'>
+                        <Link to='/login'>
+                            <FiLogIn size={22} color='#000' />
+                        </Link>
+                    </div>
+                )}
+            </header>
         </div>
     )
 }
